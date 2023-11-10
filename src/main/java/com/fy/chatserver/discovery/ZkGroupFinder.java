c44e767590e3efe4cef3d5e11277a23dd3fcc0e4
zolmk <cxy52768915>
2023-11-10 19:34:18 +0800

    feat: ...

--- src/main/java/com/fy/chatserver/discovery/ZkGroupFinder.java
+++ src/main/java/com/fy/chatserver/discovery/ZkGroupFinder.java
@@ -1,6 +1,13 @@
 package com.fy.chatserver.discovery;
 
+import cn.hutool.core.lang.UUID;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
 import org.apache.curator.shaded.com.google.common.collect.Lists;
+import org.apache.zookeeper.CreateMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -9,17 +16,29 @@ import java.util.List;
  **/
 
 public class ZkGroupFinder implements GroupFinder {
+    private static final Logger LOG = LoggerFactory.getLogger(ZkGroupFinder.class);
     private final String rootPath;
     private final String connectString;
+    private final CuratorFramework client;
 
     public ZkGroupFinder(String rootPath, String connectString) {
-        this.rootPath = rootPath;
+        this.rootPath = rootPath + "/group";
         this.connectString = connectString;
+        this.client = CuratorFrameworkFactory.newClient(connectString, 5000, 10000, new RetryNTimes(10, 200));
+        this.client.start();
     }
 
     @Override
     public String create(String uid, boolean isPublish) {
-        return null;
+        String gid = UUID.fastUUID().toString();
+        try {
+            this.client.create().withMode(CreateMode.PERSISTENT).forPath(this.rootPath + "/" + gid, isPublish ? new byte[]{1}:new byte[]{0});
+
+        } catch (Exception e) {
+            LOG.error("create group occur error.", e);
+            return "";
+        }
+        return gid;
     }
 
     @Override
---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+/**
+ * @author zhufeifei 2023/11/10
+ **/
+
+public class String {
+    @Override
+    public String toString() {
+        return "hhhh";
+    }
+}
