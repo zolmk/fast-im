dae0c6fba41f54fe552ba2da9a5aaff3655022ac
zolmk <zolmk@outlook.com>
2024-06-06 00:29:05 +0800

    feat: 完成用户级序列号发号系统收尾工作。

--- connector/src/main/java/com/feiyu/connector/Main.java
+++ connector/src/main/java/com/feiyu/connector/Main.java
@@ -3,7 +3,6 @@ package com.feiyu.connector;
 /**
  * @author zhufeifei 2024/6/2
  **/
-
 public class Main {
 
 }
---
+++ ulsequence/src/main/java/com/feiyu/ulsequence/Application.java
@@ -0,0 +1,16 @@
+package com.feiyu.ulsequence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author zhufeifei 2024/6/5
+ **/
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
