aacdbdd302d7834b2e1c0ee28559259123cd224d
zolmk <zolmk@outlook.com>
2024-06-21 18:50:37 +0800

    refactor：代码风格优化。

--- base/src/main/java/com/feiyu/base/Result.java
+++ base/src/main/java/com/feiyu/base/Result.java
@@ -6,13 +6,14 @@ import lombok.Data;
 import java.io.Serializable;
 
 /**
- * web项目中的返回结果类
+ * web项目中的返回结果类.
+ *
  * @author zhufeifei 2024/6/5
  **/
 
 @Data
 @AllArgsConstructor
-public class Result <T> implements Serializable {
-    private int code;
-    private T data;
+public class Result<T> implements Serializable {
+  private int code;
+  private T data;
 }
